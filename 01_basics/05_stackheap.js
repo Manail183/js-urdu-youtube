fefbcdd282c4d0a6b86d6b5bd99729f35be767a1
Manail183 <manailabid000@gmail.com>
2024-11-06 08:24:09 +0000

    stackheap

---
+++ 01_basics/05_stackheap.js
@@ -0,0 +1,16 @@
+// stack (primitive), heap (non-primitive)
+let myYoutubename = "manailabidcom"
+let anothername = myYoutubename
+anothername = "chaiorcode"
+console.log(myYoutubename);
+
+console.log(anothername);
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let usertwo = userone
+usertwo.email = "manail@google.com"
+console.log(userone.email);
+console.log(usertwo.email);
+
